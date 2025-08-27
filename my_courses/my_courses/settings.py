7fab0e2b4588259413d969483680fd7eed0f48de
María Alejandra Vargas Torres <sebastian_vargasmartinezst@gfc.edu.co>
2025-08-26 22:52:44 -0500

    c3

--- my_courses/my_courses/settings.py
+++ my_courses/my_courses/settings.py
@@ -46,6 +46,7 @@ DATABASES = {
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",

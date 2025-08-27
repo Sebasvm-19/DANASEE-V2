c84bd400fcc65c7e2bb8aa7f598b3e74eb8b8dcf
María Alejandra Vargas Torres <sebastian_vargasmartinezst@gfc.edu.co>
2025-08-26 21:49:37 -0500

    c3

--- my_courses/my_courses/settings.py
+++ my_courses/my_courses/settings.py
@@ -49,11 +49,15 @@ REST_FRAMEWORK = {
 }
 
 # Database
+
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL")
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",

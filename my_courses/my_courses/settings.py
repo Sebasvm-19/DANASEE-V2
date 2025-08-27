e522975bafa7f6a52e70f91d2a8e691acebeffc8
María Alejandra Vargas Torres <sebastian_vargasmartinezst@gfc.edu.co>
2025-08-26 23:44:20 -0500

    c3

--- my_courses/my_courses/settings.py
+++ my_courses/my_courses/settings.py
@@ -40,7 +40,7 @@ REST_FRAMEWORK = {
 # Database
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.getenv("DATABASE_PUBLIC_URL")
+        default="postgresql://postgres:GluWpjxMFDIJBrMEyEnKaXkuqWYlKBPJ@postgres.railway.internal:5432/railway"
     )
 }
 
